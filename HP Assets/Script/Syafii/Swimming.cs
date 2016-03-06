fb61dbd4604a2a83c2d086e45069e3d67089f32b
wowplayah <syafiiazami2013@gmail.com>
2016-03-06 17:19:03 +0700

    Update Syafii
    Update in Chapter\1\Syafii
    Update in Script\Syafii

---
+++ HP Assets/Script/Syafii/Swimming.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace HP
+{
+    public class Swimming : MonoBehaviour
+    {
+
+        RigidbodyFirstPersonController.MovementSettings MS;
+        float thrust = 3f;
+        Rigidbody rb;
+        // Use this for initialization
+        void Start()
+        {
+            RenderSettings.fog = false;
+            RenderSettings.fogColor = new Color(0.2f, 0.4f, 0.8f, 0.5f);
+            RenderSettings.fogDensity = 0.04f;
+            rb = GameObject.Find("FPSController").GetComponent<Rigidbody>();
+            MS = GameObject.Find("FPSController").GetComponent<RigidbodyFirstPersonController.MovementSettings>();
+        }
+
+        bool isUnderWater()
+        {
+            return gameObject.transform.position.y < 0.1051623;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            RenderSettings.fog = isUnderWater();
+            if (isUnderWater())
+            {
+                MS.CurrentTargetSpeed = 2f;
+                MS.JumpForce = 0f;
+                if (Input.GetKey(KeyCode.R))
+                {
+                    rb.AddForce(new Vector3(rb.velocity.x, thrust, rb.velocity.z));
+                }
+            }
+            else
+            {
+                MS.CurrentTargetSpeed = 8f;
+                MS.JumpForce = 30f;
+            }
+        }
+    }
+
+}
---
+++ HP Assets/Script/Syafii/welcome.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace HP
+{
+    public class welcome : MonoBehaviour
+    {
+        private Text textWelcome;
+        private string Welcome = "Welcome!";
+        private Canvas canvas;
+
+        // Use this for initialization
+        void Start()
+        {
+            setInitialReference();
+        }
+
+        void setInitialReference()
+        {
+            textWelcome = GameObject.Find("textWelcome").GetComponent<Text>();
+            textWelcome.text = Welcome;
+            canvas = GameObject.Find("CanvasWelcome").GetComponent<Canvas>();
+            StartCoroutine(destroyWelcome());
+        }
+
+        IEnumerator destroyWelcome()
+        {
+            yield return new WaitForSeconds(3f);
+            canvas.enabled = false;
+        }
+    }
+
+}
